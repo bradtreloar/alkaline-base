8986ece1d4072a74c546dbb69fcff0344edff47e
Brad Treloar <bradley.treloar@gmail.com>
2018-11-08 20:59:46 +1030

    Added alkaline partials

--- starterkit/gulpfile.js
+++ starterkit/gulpfile.js
@@ -1,10 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('hello', function() {
-  console.log('Hello World!');
-});
-
 // Compile .scss files to .css
 gulp.task('sass', function() {
   return gulp.src('sass/**/*.scss')
