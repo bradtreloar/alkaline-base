0817c046a1d8e9e98cf280f0f6e670909910d6c9
Brad Treloar <bradley.treloar@gmail.com>
2018-11-08 20:58:03 +1030

    Added build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,64 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+var gulp = require('gulp')
+var autoprefixer = require('gulp-autoprefixer')
+var replace = require('gulp-replace')
+var rename = require('gulp-rename')
+var sass = require('gulp-sass')
 
-gulp.task('hello', function() {
-  console.log('Hello World!');
-});
+
+// Perform all build tasks
+gulp.task('build', ['sass', 'copy-js', 'starterkit'])
+
+
+// Copy JS libraries from node modules to JS folder.
+gulp.task('copy-js', () => {
+  return gulp.src([
+      "./node_modules/foundation-sites/dist/js/foundation.min.js",
+      "./node_modules/motion-ui/dist/motion-ui.min.js",
+    ])
+    .pipe(gulp.dest("./js"))
+})
+
+
+// Copy SASS components to starterkit as partials.
+gulp.task('starterkit', () => {
+  return gulp.src([
+      "./sass/base/typography.scss",
+      "./sass/components/*.scss",
+      "./sass/layout/*.scss",
+      "./sass/util/*.scss",
+      "./sass/_global.scss"
+    ], {
+      base: './sass'
+    })
+    .pipe(replace("@import 'global';", ""))
+    .pipe(replace(/@if \$theme [^]*/, ""))
+    .pipe(rename((path) => {
+      // Add leading underscore to filename to change file to SASS partial.
+      if (!path.basename.includes("_"))
+        path.basename = "_" + path.basename;
+    }))
+    .pipe(gulp.dest("./starterkit/sass/alkaline"))
+})
+
 
 // Compile .scss files to .css
-gulp.task('sass', function() {
-  return gulp.src('sass/**/*.scss')
+gulp.task('sass', () => {
+  return gulp.src("./sass/**/*.scss")
     .pipe(sass({
       includePaths: [
-        './node_modules',
-        './sass',
+        "./node_modules",
+        "./sass",
       ],
     }))
     .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
+      browsers: ["last 2 versions"],
       cascade: false
     }))
-    .pipe(gulp.dest('css'))
-});
+    .pipe(gulp.dest("./css"))
+})
+
 
 // Compile .scss files to .css when .scss files are updated.
-gulp.task('watch', function() {
-  gulp.watch('sass/**/*.scss', ['sass']); 
-});
+gulp.task('watch', () => {
+  gulp.watch("./sass/**/*.scss", ['sass']) 
+})
