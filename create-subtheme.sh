1cc7f0940769eb5d6a0ea9f516e339ec61cf04bc
Brad Treloar <bradley.treloar@gmail.com>
2018-11-12 20:23:51 +1030

    Various changes

--- create-subtheme.sh
+++ create-subtheme.sh
@@ -7,6 +7,7 @@ SUBTHEME_NAME=$1
 STARTERKIT_PATH="$(pwd)/starterkit"
 
 # Copy starterkit to subtheme.
+mkdir -p ../../custom/$SUBTHEME_NAME
 cp -r $STARTERKIT_PATH/* ../../custom/$SUBTHEME_NAME
 
 # Move to subtheme directory.
@@ -18,4 +19,4 @@ mv starterkit.info.yml.txt $SUBTHEME_NAME.info.yml
 mv starterkit.theme $SUBTHEME_NAME.theme
 
 # Find and replace "starterkit" with subtheme name
-for file in $(find . -type f); do sed s/starterkit/$SUBTHEME_NAME/ $file; done
+#for file in $(find . -type f); do sed s/starterkit/$SUBTHEME_NAME/ $file; done
