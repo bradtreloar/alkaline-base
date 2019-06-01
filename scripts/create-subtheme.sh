be189146bec9cb15d16dadc29bc1e82bc80fc465
Brad Treloar <brad@treloardigital.com.au>
2019-06-02 00:50:47 +0930

    finished subtheme and moved to scripts folder

--- scripts/create-subtheme.sh
+++ scripts/create-subtheme.sh
@@ -13,10 +13,18 @@ cp -r $STARTERKIT_PATH/* ../../custom/$SUBTHEME_NAME
 # Move to subtheme directory.
 cd ../../custom/$SUBTHEME_NAME
 
-# Rename theme files
+# Rename theme files.
 mv starterkit.libraries.yml $SUBTHEME_NAME.libraries.yml
 mv starterkit.info.yml.txt $SUBTHEME_NAME.info.yml
 mv starterkit.theme $SUBTHEME_NAME.theme
 
-# Find and replace "starterkit" with subtheme name
-#for file in $(find . -type f); do sed s/starterkit/$SUBTHEME_NAME/ $file; done
+# Find and replace "starterkit" with subtheme name.
+for file in $(find . -type f); do sed -i s/starterkit/$SUBTHEME_NAME/ $file; done
+
+# Find and replace "STARTERKIT" with subtheme name.
+for file in $(find . -type f); do sed -i s/STARTERKIT/$SUBTHEME_NAME/ $file; done
+
+# Rename config files.
+for file in $(find ./config/ -type f); do 
+  mv $file $(echo $file | sed -e "s/starterkit/$SUBTHEME_NAME/")
+done
