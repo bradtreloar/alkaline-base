d178edb2374013c78f2a3cd0b368f58e91e45e1d
Brad Treloar <brad@treloardigital.com.au>
2019-11-02 12:09:27 +1030

    Simplified build tooling

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,60 @@
 const gulp = require("gulp");
 const autoprefixer = require("gulp-autoprefixer");
+const babel = require("gulp-babel");
 const replace = require("gulp-replace");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 
+// Compile .scss files to .css
+gulp.task("build:css", () =>
+  gulp
+    .src("./sass/**/*.scss")
+    .pipe(
+      sass({
+        includePaths: ["./node_modules", "./sass"]
+      })
+    )
+    .pipe(
+      autoprefixer({
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest("./css"))
+);
+
+// Compile .scss files to .css
+gulp.task("build:js", () =>
+  gulp
+    .src("./js/**/*.es6.js")
+    .pipe(
+      babel({
+        presets: ["@babel/env"]
+      })
+    )
+    .pipe(
+      rename(path => {
+        // Replace the ".es6.js" extension with just ".js".
+        path.basename = path.basename.replace(".es6", "");
+      })
+    )
+    .pipe(gulp.dest("./js"))
+);
+
+// Copy CSS libraries from node modules to CSS folder.
+gulp.task("copy:css", () =>
+  gulp
+    .src(["./node_modules/motion-ui/dist/motion-ui.min.css"])
+    .pipe(gulp.dest("./css"))
+);
+
 // Copy JS libraries from node modules to JS folder.
-gulp.task("copy-js", () =>
+gulp.task("copy:js", () =>
   gulp
     .src([
       "./node_modules/foundation-sites/dist/js/foundation.min.js",
       "./node_modules/motion-ui/dist/motion-ui.min.js"
     ])
     .pipe(gulp.dest("./js"))
-);
-
-// Copy CSS libraries from node modules to CSS folder.
-gulp.task("copy-css", () =>
-  gulp
-    .src(["./node_modules/motion-ui/dist/motion-ui.min.css"])
-    .pipe(gulp.dest("./css"))
 );
 
 // Copy SASS components to starterkit as partials.
@@ -47,28 +83,18 @@ gulp.task("starterkit", () =>
     .pipe(gulp.dest("./starterkit/sass/alkaline"))
 );
 
-// Compile .scss files to .css
-gulp.task("sass", () =>
-  gulp
-    .src("./sass/**/*.scss")
-    .pipe(
-      sass({
-        includePaths: ["./node_modules", "./sass"]
-      })
-    )
-    .pipe(
-      autoprefixer({
-        browsers: ["last 2 versions"],
-        cascade: false
-      })
-    )
-    .pipe(gulp.dest("./css"))
-);
+// Compile .scss files to .css when .scss files are updated.
+gulp.task("watch:css", () => {
+  gulp.watch("./sass/**/*.scss", gulp.parallel("build:css"));
+});
 
 // Compile .scss files to .css when .scss files are updated.
-gulp.task("watch", () => {
-  gulp.watch("./sass/**/*.scss", gulp.parallel("sass"));
+gulp.task("watch:js", () => {
+  gulp.watch("./js/**/*.es6.js", gulp.series("build:js"));
 });
 
 // Perform all build tasks
-gulp.task("build", gulp.parallel("sass", "copy-css", "copy-js", "starterkit"));
+gulp.task(
+  "build",
+  gulp.parallel("build:css", "build:js", "copy:css", "copy:js", "starterkit")
+);
